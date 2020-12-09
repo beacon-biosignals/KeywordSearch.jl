7323c4436a9259b6f14a87ac7ad5e98f940719c2
Eric Hanson <5846501+ericphanson@users.noreply.github.com>
2020-12-09 18:50:31 +0100

    use `AUTOMATIC_REPLACEMENTS` instead of separate `punct_preprocess` function (#14)
    * use `AUTOMATIC_REPLACEMENTS` for punctuation preprocessing
    
    * format

--- src/KeywordSearch.jl
+++ src/KeywordSearch.jl
@@ -7,14 +7,27 @@ using Tables
 export DamerauLevenshtein, FuzzyQuery, Query, Corpus, Document
 export augment, explain, match_all, word_boundary, NamedQuery
 
-"""
-    const AUTOMATIC_REPLACEMENTS::Vector{Pair{String, String}}
+@doc raw"""
+    const AUTOMATIC_REPLACEMENTS::Vector{Pair{Union{Regex,String},String}}
 
 A list of replacements to automatically perform when preprocessing a [`Document`](@ref).
 For example, if `KeywordSearch.AUTOMATIC_REPLACEMENTS == ["a" => "b"]`, then
 `Document("abc").text == "bbc"` instead of "abc".
+
+By default, `AUTOMATIC_REPLACEMENTS` contains only one replacement,
+
+```julia
+r"[.!?><\-\n\r\v\t\f]" => " "
+```
+
+which replaces certain punctuation characters, whitespace, and newlines with a space.
+This replacement is needed for [`word_boundary`](@ref) to work correctly, but you
+can remove it with `empty!(KeywordSearch.AUTOMATIC_REPLACEMENTS)` if you wish.
+
+You an also add other preprocessing directives by `push!`ing further replacements
+into `KeywordSearch.AUTOMATIC_REPLACEMENTS`.
 """
-const AUTOMATIC_REPLACEMENTS = Pair{String,String}[]
+const AUTOMATIC_REPLACEMENTS = Pair{Union{Regex,String},String}[r"[.!?><\-\n\r\v\t\f]" => " "]
 
 include("core.jl")
 include("corpus.jl")
