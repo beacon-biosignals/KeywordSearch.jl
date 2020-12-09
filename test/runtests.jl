2cd93832ddadbd56808e3bec8a6d73957b43041e
Eric Hanson <5846501+ericphanson@users.noreply.github.com>
2020-12-09 17:15:35 +0100

    extend table interface for `NamedMatch`es (#7)
    * extend table interface
    
    * improve coverage

--- test/runtests.jl
+++ test/runtests.jl
@@ -401,11 +401,17 @@ end
     @test match(Q, D4).metadata == (; query_name="other", document_uuid=d_uuid)
 
     @testset "Tables interface for `NamedMatch`s" begin
+        @test Tables.getcolumn(res, 1) == Tables.getcolumn(res, :query) == Q.query
+        @test Tables.getcolumn(res, 2) == Tables.getcolumn(res, :haystack) == D4
+        @test Tables.getcolumn(res, 3) == Tables.getcolumn(res, :distance) == 0
+        @test Tables.getcolumn(res, 4) == Tables.getcolumn(res, :indices)
+        @test Tables.getcolumn(res, 5) == Tables.getcolumn(res, :query_name) == "other"
+        @test Tables.getcolumn(res, 6) == Tables.getcolumn(res, :corpus_uuid) == C2_uuid
         tbl = [res, res]
-        @test Tables.getcolumn(res, 2) == "other"
         @test Tables.isrowtable(tbl)
         @test Tables.columns(tbl).query_name == ["other", "other"]
-        @test Tables.columnnames(res) == (:match, :query_name, :corpus_uuid, :document_uuid)
+        @test Tables.columnnames(res) == (:haystack, :distance, :indices, :query, :query_name, :corpus_uuid, :document_uuid)
+        @test sprint(explain, (first(Tables.rowtable(tbl)))) === "The query \" other\" exactly matched the text \"There were other cobras \".\n"
     end
 end
 
